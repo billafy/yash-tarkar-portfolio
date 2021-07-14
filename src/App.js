7af1fb6348f9f5e3421d641a1b88eb96fdcda3ec
unknown <ytarkar138@gmail.com>
2021-07-14 22:56:24 +0530

    changed-skill-icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import BooksApp from './books-app/BooksApp';
 import Portfolio from './portfolio-components/Portfolio';
-import SendImage from './send-images/SendImage';
 
 const App = () => {
   return <Portfolio/>;
